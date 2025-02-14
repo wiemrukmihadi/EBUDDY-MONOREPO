d9afd55b873da1249b14d86ca4a03d0c4412e788
Wiem Rukmihadi <wiemrukmihadi@jukesolutions.com>
2025-02-15 01:04:47 +0700

    initial

---
+++ packages/shared/src/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    totalAverageWeightRatings: number;
+    numberOfRents: number;
+    recentlyActive: number;
+    rankingScore?: number;
+  }
